0689b05582c2cb2d5c748a09bf0989a2c42d1bbf
Luke Wahlmeier <luke2015@ksu.edu>
2020-12-28 21:22:14 -0600

    Added Material UI for Icons and UX/UI tools
    Added Material UI to help with using free icons and to help with making a responsive design with the Grid functionality.

--- portfolio-app/src/App.js
+++ portfolio-app/src/App.js
@@ -1,14 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import Grid from '@material-ui/core/Grid';
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>LukeHW</h1>
-        <h2>
-So today, for the first time, my little toddler finally counted to ten. Everyone was celebrating, saying how proud they are in my kid, and then Ben Shapiro kicks open the door. "Oh you think it's impressive that they can count to ten? I can count to one million." and then proceeded, in my living room for the next two weeks, to count to one million. He then said "yep, another libtard destroyed" and then curbstomped my kid.</h2>
+        <h2>So today, for the first time, my little toddler finally counted to ten. Everyone was celebrating, saying how proud they are in my kid, and then Ben Shapiro kicks open the door. "Oh you think it's impressive that they can count to ten? I can count to one million." and then proceeded, in my living room for the next two weeks, to count to one million. He then said "yep, another libtard destroyed" and then curbstomped my kid.</h2>
       </header>
+      <Grid container spacing={5} style={{padding: '24px'}}>
+        <Grid item xs={12} sm={6} md={4} lg={4} xl={4}>
+        So today, for the first time, my little toddler finally counted to ten. Everyone was celebrating, saying how proud they are in my kid, and then Ben Shapiro kicks open the door. "Oh you think it's impressive that they can count to ten? I can count to one million." and then proceeded, in my living room for the next two weeks, to count to one million. He then said "yep, another libtard destroyed" and then curbstomped my kid.
+        </Grid>
+        <Grid item xs={12} sm={6} md={4} lg={4} xl={4}>
+        So today, for the first time, my little toddler finally counted to ten. Everyone was celebrating, saying how proud they are in my kid, and then Ben Shapiro kicks open the door. "Oh you think it's impressive that they can count to ten? I can count to one million." and then proceeded, in my living room for the next two weeks, to count to one million. He then said "yep, another libtard destroyed" and then curbstomped my kid.
+        </Grid>
+        <Grid item xs={12} sm={6} md={4} lg={4} xl={4}>
+        So today, for the first time, my little toddler finally counted to ten. Everyone was celebrating, saying how proud they are in my kid, and then Ben Shapiro kicks open the door. "Oh you think it's impressive that they can count to ten? I can count to one million." and then proceeded, in my living room for the next two weeks, to count to one million. He then said "yep, another libtard destroyed" and then curbstomped my kid.
+        </Grid>
+      </Grid>
     </div>
   );
 }
