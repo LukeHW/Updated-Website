3268861cc54537ffe1b504ec82dddcc8602fe3a0
Luke Wahlmeier <luke2015@ksu.edu>
2020-12-28 22:19:22 -0600

    Adjusted some Grid components for testing cards
    Added responsiveness to website. Can do adjustments later.
    -Main ideas are to use the grid system to display content in a easy-to-manage way

--- portfolio-app/src/App.js
+++ portfolio-app/src/App.js
@@ -1,8 +1,25 @@
+import React from 'react';
+import Paper from '@material-ui/core/Paper';
+import { makeStyles } from '@material-ui/core/styles';
+import Avatar from '@material-ui/core/Avatar';
+import Typography from '@material-ui/core/Typography';
 import logo from './logo.svg';
 import './App.css';
 import Grid from '@material-ui/core/Grid';
 
+const useStyles = makeStyles((theme) => ({
+  container: {
+    maxWidth: 1600,
+    justifyContent: 'space-between'
+  },
+  item: {
+    padding: "20px"
+  }
+}));
+
 function App() {
+
+  const classes = useStyles();
 
   return (
     <div className="App">
@@ -10,14 +27,14 @@ function App() {
         <h1>LukeHW</h1>
         <h2>So today, for the first time, my little toddler finally counted to ten. Everyone was celebrating, saying how proud they are in my kid, and then Ben Shapiro kicks open the door. "Oh you think it's impressive that they can count to ten? I can count to one million." and then proceeded, in my living room for the next two weeks, to count to one million. He then said "yep, another libtard destroyed" and then curbstomped my kid.</h2>
       </header>
-      <Grid container spacing={5} style={{padding: '24px'}}>
-        <Grid item xs={12} sm={6} md={4} lg={4} xl={4}>
+      <Grid container className={classes.container}>
+        <Grid item className={classes.item} xs={12} sm={6} md={6} lg={4} xl={4}>
         So today, for the first time, my little toddler finally counted to ten. Everyone was celebrating, saying how proud they are in my kid, and then Ben Shapiro kicks open the door. "Oh you think it's impressive that they can count to ten? I can count to one million." and then proceeded, in my living room for the next two weeks, to count to one million. He then said "yep, another libtard destroyed" and then curbstomped my kid.
         </Grid>
-        <Grid item xs={12} sm={6} md={4} lg={4} xl={4}>
+        <Grid item className={classes.item} xs={12} sm={6} md={6} lg={4} xl={4}>
         So today, for the first time, my little toddler finally counted to ten. Everyone was celebrating, saying how proud they are in my kid, and then Ben Shapiro kicks open the door. "Oh you think it's impressive that they can count to ten? I can count to one million." and then proceeded, in my living room for the next two weeks, to count to one million. He then said "yep, another libtard destroyed" and then curbstomped my kid.
         </Grid>
-        <Grid item xs={12} sm={6} md={4} lg={4} xl={4}>
+        <Grid item className={classes.item} xs={12} sm={12} md={12} lg={4} xl={4}>
         So today, for the first time, my little toddler finally counted to ten. Everyone was celebrating, saying how proud they are in my kid, and then Ben Shapiro kicks open the door. "Oh you think it's impressive that they can count to ten? I can count to one million." and then proceeded, in my living room for the next two weeks, to count to one million. He then said "yep, another libtard destroyed" and then curbstomped my kid.
         </Grid>
       </Grid>
