eb15cdff26375816f20c4f6741a515135ed68f4c
Luke Wahlmeier <luke2015@ksu.edu>
2020-12-31 23:03:06 -0600

    Last commit of day
    Finalizing look for end of day.

--- portfolio-app/src/App.js
+++ portfolio-app/src/App.js
@@ -12,7 +12,7 @@ import bg from './blurredBG.jpg';
 const useStyles = makeStyles((theme) => ({
   container: {
     justifyContent: "space-between",
-    marginTop: "100px"
+    marginTop: "300px"
   },
   item: {
     padding: "20px",
@@ -23,6 +23,13 @@ const useStyles = makeStyles((theme) => ({
     backgroundImage: `url(${bg})`,
     backgroundRepeat: "no-repeat center",
     backgroundSize: "cover"
+  },
+  title: {
+    fontWeight: 700,
+    fontSize: 100
+  },
+  greeting: {
+    textAlign: "left"
   }
 }));
 
@@ -36,14 +43,11 @@ function App() {
       <div className="AppBG">
         <Navbar/>
         <Grid container className={classes.container}>
-          <Grid item className={classes.item} xs={12} sm={12} md={12} lg={12} xl={12}>
-          <Typography variant="h1" component="h2" gutterBottom>
-                    Luke H. Wahlmeier
-          </Typography>
-          </Grid>
           <Grid xs={0} sm={0} md={3} lg={3} xl={3}></Grid> 
-          <Grid item className={classes.item} xs={12} sm={12} md={6} lg={6} xl={6}>
-          So today, for the first time, my little toddler finally counted to ten. Everyone was celebrating, saying how proud they are in my kid, and then Ben Shapiro kicks open the door. "Oh you think it's impressive that they can count to ten? I can count to one million." and then proceeded, in my living room for the next two weeks, to count to one million. He then said "yep, another libtard destroyed" and then curbstomped my kid.
+          <Grid item className={classes.item} xs={12} sm={12} md={12} lg={12} xl={12}>
+            <Typography className={classes.title} variant="h1" component="h2" gutterBottom>
+              LUKE H. WAHLMEIER
+            </Typography>
           </Grid>
           <Grid xs={0} sm={0} md={3} lg={3} xl={3}></Grid> 
         </Grid>
