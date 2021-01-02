e69d7f64fa23155059cd65c975f8cb5c4a94dbba
Luke Wahlmeier <luke2015@ksu.edu>
2021-01-01 21:07:57 -0600

    Added burger menu icon for smaller screen sizes
    Added burger menu icon for smaller screens. Stays hidden on larger screens, while other menu options reside in navbar. Vice versa for smaller screens.

--- portfolio-app/src/Navbar.js
+++ portfolio-app/src/Navbar.js
@@ -7,6 +7,8 @@ import logo from './logo.svg';
 import './App.css';
 import Grid from '@material-ui/core/Grid';
 import Hidden from '@material-ui/core/Hidden';
+import MenuIcon from '@material-ui/icons/Menu';
+import IconButton from '@material-ui/core/IconButton';
 
 const useStyles = makeStyles((theme) => ({
   navbarLogo: {
@@ -24,6 +26,10 @@ const useStyles = makeStyles((theme) => ({
       color: "White",
       marginTop: 25,
       textAlign: "center"
+  },
+  menuIcon: {
+      fontSize: 50,
+      color: "white"
   }
 }));
 
@@ -34,26 +40,36 @@ function Navbar() {
   return (
     <div className={classes.navbarContainer}>
         <Grid container className={classes.container}>
-            <Grid xs={1} sm={2} md={2} lg={3} xl={3}></Grid> 
+            <Grid xs={1} sm={1} md={2} lg={3} xl={3}></Grid> 
             <Grid item className={classes.logo} xs={1} sm={1} md={1} lg={1} xl={1}>
                 <img className={classes.navbarLogo} src="/favicon.png" alt="LHW logo"/>
             </Grid>
             <Grid xs={1} sm={1} md={1} lg={2} xl={2}></Grid> 
-            <Grid item className={classes.link} xs={3} sm={2} md={2} lg={1} xl={1}>
-                <Typography variant="h5" component="h2" gutterBottom>
-                    Work
-                </Typography>
-            </Grid>
-            <Grid item className={classes.link} xs={3} sm={2} md={2} lg={1} xl={1}>
-                <Typography variant="h5" component="h2" gutterBottom>
-                    About
-                </Typography>
-            </Grid>
-            <Grid item className={classes.link} xs={3} sm={2} md={2} lg={1} xl={1}>
-                <Typography variant="h5" component="h2" gutterBottom>
-                    Contact
-                </Typography>
-            </Grid>
+            <Hidden smDown>
+                <Grid item className={classes.link} xs={3} sm={2} md={2} lg={1} xl={1}>
+                    <Typography variant="h5" component="h2" gutterBottom>
+                        Work
+                    </Typography>
+                </Grid>
+                <Grid item className={classes.link} xs={3} sm={2} md={2} lg={1} xl={1}>
+                    <Typography variant="h5" component="h2" gutterBottom>
+                        About
+                    </Typography>
+                </Grid>
+                <Grid item className={classes.link} xs={3} sm={2} md={2} lg={1} xl={1}>
+                    <Typography variant="h5" component="h2" gutterBottom>
+                        Contact
+                    </Typography>
+                </Grid>
+            </Hidden>
+            <Hidden mdUp>
+                <Grid xs={7} sm={7}></Grid> 
+                <Grid item className={classes.menuContainer} xs={2} sm={2}>
+                    <IconButton aria-label="menu">
+                        <MenuIcon className={classes.menuIcon}/>
+                    </IconButton>
+                </Grid>
+            </Hidden>
         </Grid>
     </div>
   );
